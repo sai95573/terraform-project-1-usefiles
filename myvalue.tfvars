79c90f035d264e8046d60c2c2468093a659cda17
saikrishna <sai95573@gmail.com>
2023-04-27 17:33:14 +0530

    tera use files added

---
+++ myvalue.tfvars
@@ -0,0 +1,15 @@
+my_aws_region = "your-region"
+my_access_key = ""
+my_secret_key = ""
+my_domain_name = ""
+my_web_sub_domain_name = "web"
+my_mail_sub_domain_name = "mail"
+my_web_sub_domain_name = "db"
+my_local_aws_private_key_path = ""
+my_key_name = ""
+my_web_want = false
+my_web_count = 1
+my_mail_want = true
+my_mail_count = 4
+my_db_want = false
+my_db_count = 1
---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "my_aws_region" {
+    type = string
+    description = "This variable is used to specify region"
+}
+
+variable "my_access_key" {
+    type = string
+    description = "This variable is used to specify access_key"
+}
+
+variable "my_secret_key" {
+    type = string
+    description = "This variable is used to specify access_key"
+}
+
+variable "my_domain_name" {
+    type = string
+}
+
+variable "my_web_sub_domain_name" {
+    type = string
+}
+
+
+variable "my_mail_sub_domain_name" {
+    type = string
+}
+
+
+variable "my_local_aws_private_key_path" {
+    type = string
+  
+}
+
+variable "my_web_want" {
+    type = bool
+
+}
+
+variable "my_web_count" {
+    type = number
+  
+}
+
+variable "my_mail_want" {
+    type = bool
+
+}
+
+variable "my_mail_count" {
+    type = number
+      
+}
+
+variable "my_key_name" {
+    type = string 
+}
+
+variable "my_db_want" {
+    type = bool
+
+}
+
+variable "my_db_count" {
+    type = number
+  
+}
